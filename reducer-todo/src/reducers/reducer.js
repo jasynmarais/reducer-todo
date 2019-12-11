8acd269b31467a15c0cb40c7cf75d8a6b2928d53
Jasyn Marais <jasynmarais@gmail.com>
2019-12-11 15:37:00 +0200

    Export reducer and initial state

---
+++ reducer-todo/src/reducers/reducer.js
@@ -0,0 +1,24 @@
+export function reducer(state, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export const initialState = [
+    {
+        item: 'Learn about reducers',
+        completed: false,
+        id: 3892987589
+      },
+      {
+        item: 'Learn about redux',
+        completed: false,
+        id: 234234234
+      },
+      {
+        item: 'Learn about actions',
+        completed: false,
+        id: 123123311
+      }
+];
